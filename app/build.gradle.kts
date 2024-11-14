de9f25af36415ca63583def6359c331f06a2f3c1
4rcanine <145784416+4rcanine@users.noreply.github.com>
2024-11-14 22:10:57 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.material)
 }
