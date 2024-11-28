1e069bc19d005ca9fd4ba56cf5bbcf302643b285
4rcanine <145784416+4rcanine@users.noreply.github.com>
2024-11-28 10:50:23 +0800

    yung transparent na sinasabi mo tas inayos ko na din yung toolbar

--- app/src/main/java/com/example/museraya/HomeActivity.kt
+++ app/src/main/java/com/example/museraya/HomeActivity.kt
@@ -1,8 +1,10 @@
 package com.example.museraya
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -24,6 +26,11 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                )
+        window.statusBarColor = Color.TRANSPARENT
 
         // Toolbar setup
         val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
