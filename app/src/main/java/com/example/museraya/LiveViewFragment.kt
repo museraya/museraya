11ecd7d7ad18229abbdb89b02931046f2c2bc843
williamcunanan <wylliamcunanan@gmail.com>
2024-12-13 22:20:40 +0800

    pakitry kung gagana ung sa AR ngayon

--- app/src/main/java/com/example/museraya/LiveViewFragment.kt
+++ app/src/main/java/com/example/museraya/LiveViewFragment.kt
@@ -82,7 +82,7 @@ class LiveViewFragment : Fragment() {
                 isEditable = true
                 lifecycleScope.launch {
                     val modelInstance = arSceneView.modelLoader.loadModelInstance(
-                        "file:///android_asset/models/rubber_duck_toy_4k.gltf"
+                        "file:///android_asset/models/woodcutter.glb"
                     )
                     if (modelInstance != null) {
                         Toast.makeText(requireContext(), "Model loaded successfully", Toast.LENGTH_SHORT).show()
