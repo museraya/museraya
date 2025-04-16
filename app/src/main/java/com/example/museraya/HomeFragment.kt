f8ef91719c9834183ed99706d2b906adf3722a14
4rcanine <145784416+4rcanine@users.noreply.github.com>
2025-04-16 21:42:02 +0800

    inayos yung UI ng amin ng lahat nagdagdag ng Misc na walang laman

--- app/src/main/java/com/example/museraya/HomeFragment.kt
+++ app/src/main/java/com/example/museraya/HomeFragment.kt
@@ -6,7 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
+import androidx.navigation.findNavController // Can keep this or use the extension
+import androidx.navigation.fragment.findNavController // Cleaner extension
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.google.android.material.appbar.MaterialToolbar // Import MaterialToolbar
@@ -22,41 +23,35 @@ class HomeFragment : Fragment() {
 
         // --- Toolbar Setup ---
         val toolbar: MaterialToolbar = view.findViewById(R.id.toolbar)
-        // If you want the toolbar to have navigation icon or options menu, set it up
         // (activity as? AppCompatActivity)?.setSupportActionBar(toolbar)
-        // (activity as? AppCompatActivity)?.supportActionBar?.setDisplayHomeAsUpEnabled(true) // Example
 
         // --- RecyclerView Setup ---
         val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
-
-        // Use StaggeredGridLayoutManager
-        val spanCount = 2 // Number of columns
+        val spanCount = 2
         recyclerView.layoutManager =
             StaggeredGridLayoutManager(spanCount, StaggeredGridLayoutManager.VERTICAL)
 
-        // Make sure your images actually have different aspect ratios for stagger effect!
-        // If all drawables d1, d2, d3, d4 are the same size/shape, it will look like a grid.
+        // --- Reorder the catalogItems list ---
         val catalogItems = listOf(
             CatalogItem("Vintage Audio", R.drawable.d1),
             CatalogItem("Vintage Film Camera Collection", R.drawable.d2),
+            // Miscellaneous moved lower
             CatalogItem("Vintage Music", R.drawable.d3),
-            CatalogItem("Traditional Arts", R.drawable.d4)
+            CatalogItem("Traditional Arts", R.drawable.d4),
+            CatalogItem("Miscellaneous", R.drawable.misc)
         )
 
         val adapter = CatalogAdapter(catalogItems) { clickedItem ->
-            val navController = view.findNavController()
+            val navController = findNavController() // Use fragment extension
             when (clickedItem.title) {
-                // Make sure titles here EXACTLY match the ones in catalogItems
                 "Vintage Audio" -> navController.navigate(R.id.navigation_vintage_radio)
                 "Vintage Film Camera Collection" -> navController.navigate(R.id.vintageFilmFragment)
                 "Vintage Music" -> navController.navigate(R.id.vintageMusicFragment)
                 "Traditional Arts" -> navController.navigate(R.id.artFragment)
+                "Miscellaneous" -> navController.navigate(R.id.navigation_miscellaneous) // Navigation case remains the same
             }
         }
         recyclerView.adapter = adapter
-
-        // --- Important for NestedScrollView performance ---
-        // This helps NestedScrollView fling correctly with RecyclerView
         recyclerView.isNestedScrollingEnabled = false
 
         return view
